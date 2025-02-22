fcf2ad176db9486905ff21589ea371d9cb7c528b
johnarthurmellor <kushmaksim66@gmail.com>
2025-02-22 15:11:00 +0300

    init: initialize project

---
+++ diploma_app/app/Providers/AppServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Vite::prefetch(concurrency: 3);
+
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->greeting('Здравствуйте!')
+                ->subject('Подтвердите адрес электронной почты')
+                ->line('Нажмите кнопку ниже, чтобы подтвердить свой адрес электронной почты.')
+                ->action('Подтвердите адрес электронной почты', $url)
+                ->salutation('С уважением, команда сайта');
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $url = url(route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+
+            return (new MailMessage)
+                ->subject('Сброс пароля')
+                ->greeting('Здравствуйте!')
+                ->line('Вы получили это письмо, так как мы получили запрос на сброс пароля для вашей учетной записи.')
+                ->action('Сбросить пароль', $url)
+                ->line('Ссылка для сброса пароля истечёт через '
+                    . config('auth.passwords.'.config('auth.defaults.passwords').'.expire')
+                    . ' минут.')
+                ->line('Если вы не запрашивали сброс пароля, никаких действий предпринимать не нужно.')
+                ->salutation('С уважением, команда сайта');
+        });
+    }
+}
---
+++ diploma_app/lang/ru/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Ваш пароль был успешно изменен.',
+    'sent' => 'Мы отправили вам ссылку для сброса пароля.',
+    'throttled' => 'Пожалуйста, подождите перед повторной попыткой.',
+    'token' => 'Ссылка для сброса пароля недействительна.',
+    'user' => 'Пользователь с таким адресом электронной почты не найден.',
+];
